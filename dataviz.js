3d21f8dc85dff1a0c5406a17483ebd154e236193
Michael Bruening <m.bruening@cademica.net>
2025-11-16 18:30:45 +0100

    new: compare 3 rounds: added ctrl-click to display details using 3rd table

--- dataviz.js
+++ dataviz.js
@@ -11,6 +11,7 @@
   const rundenGraph   = document.getElementById( "rundenGraph" );
   const rundenInfo    = document.getElementById( "rundenInfo" );
   const passeInfoLeft = document.getElementById( "passeInfoLeft" );
+  const passeInfoMid  = document.getElementById( "passeInfoMid" );
   const passeInfoRight= document.getElementById( "passeInfoRight" );
   let TrainingChart;
   let RundenChart;
@@ -149,6 +150,8 @@
       timeline.setSelection('');
       showRundenInfo( {"items": [ visibleItems ]});
       showPasseInfoLeft( 0 );
+      showPasseInfoMid( 0 );
+      showPasseInfoRight( 0 );
     });
     return timeline;
   };
@@ -224,7 +227,7 @@
       tbody.rows[i].insertCell(0).innerText = i+1;
       for( let j=1; j< rundenCols.length; j++){
 	if( j===1 ){
-        	tbody.rows[i].insertCell(j).innerHTML = '<button onmouseover="showPasseInfo(['+RDdata[i].rid+'],['+(i+1)+'])">'+Runden[rundenCols[j]]+'</button>';
+		tbody.rows[i].insertCell(j).innerHTML = '<button onmouseover="showPasseInfoLeft(['+RDdata[i].rid+'],['+(i+1)+'])">'+Runden[rundenCols[j]]+'</button>';
 	} else {
 		tbody.rows[i].insertCell(j).innerText = Runden[rundenCols[j]];
 	}
@@ -250,7 +253,7 @@
         sumMax += RDdata[i].max;
       }
       sumPercent = (sumPoints/sumMax)*100;
-      return 'Alle ' + RDdata.length + ' Runden: ' + sumPercent.toFixed(0) + "% (" + sumPoints + ' von ' + sumMax + ") ";
+      return ['Alle ' + RDdata.length + ' Runden: ' + sumPercent.toFixed(0) + "% (" + sumPoints + ' von ' + sumMax + ") ", 'Click to show Runden-Details (left)', 'Shift-Click: compare (middle)', 'Ctrl-Click:  compare (right)'];
     }
 
     if( RundenChart ){ RundenChart.destroy(); }
@@ -291,11 +294,12 @@
           }
         },
         onClick: function(c,i) {
-//console.log("c: %o", c);
           var e = i[0];
           if( c.native.shiftKey ) {
+            showPasseInfoMid( RDdata[e.index].rid, i[0].index+1);
+          } else if( c.native.ctrlKey  ) { 
             showPasseInfoRight( RDdata[e.index].rid, i[0].index+1 );
-          }else{
+          } else {
             showPasseInfoLeft( RDdata[e.index].rid, i[0].index+1 );
           }
         }
@@ -381,6 +385,85 @@
     }
   };
   window.showPasseInfoLeft = showPasseInfoLeft;
+
+  function showPasseInfoMid (rid, rnum) {
+    passeInfoMid.innerHTML = '';
+    var fLocation="";
+    var selected = document.getElementById("selLocation").value;
+    selected == "" ? fLocation="" : fLocation=" AND location='" + selected + "'";
+    var fDistance="";
+    var selected = document.getElementById("selDistance").value;
+    selected == "" ? fDistance="" : fDistance=" AND distance='" + selected + "'";
+    var fTarget="";
+    var selTarget = document.getElementById("selTarget").value;
+    selTarget == "" ? fTarget="" : fTarget=" AND targetDiameter='" + selTarget + "'";
+    var stmt = db.prepare("\
+      SELECT T.date AS Datum, T.location AS Ort, distance AS Distanz, E.reachedPoints AS Punkte, \
+        E.totalPoints AS Max, round(((E.reachedPoints*1.0)/(E.totalPoints*1.0)*100)) AS Prozent, E.shotCount AS Schuss, \
+        S.scoringRing AS Ring, round(S.x,2) AS xPos, round(S.y,2) AS yPos \
+      FROM Shot AS S, End AS E, Round AS R, Training AS T \
+      WHERE S.endId=E.id AND E.roundId = R.id AND R.trainingId = T.id AND R.id IN (" + rid + ")" + fDistance + fTarget + fLocation + "\
+      GROUP BY S.id, E.id, R.id \
+      ORDER BY date ASC");
+
+    const passeTable = document.createElement("TABLE");
+    passeTable.classList.add('ChartTable');
+    const thead = passeTable.createTHead();
+    thead.classList.add('ChartTableHead');
+    thead.insertRow(0);
+    const passeCols = [ '#'+rnum, 'Datum', 'Ort', 'Distanz', 'Punkte', 'Max', 'Prozent', 'Schuss', '1', '2', '3', '4', '5', '6' ];
+    for( let i=0; i< passeCols.length; i++){
+      thead.rows[0].insertCell(i).innerText = passeCols[i];
+    };
+
+    const tbody = passeTable.createTBody();
+    tbody.classList.add('ChartTableBody');
+    let i=0;
+    let c=0;
+    let r=[];
+    let x=[];
+    let y=[];
+    while(stmt.step()) {
+      var Runden = stmt.getAsObject();
+      // prepare Graph
+
+      // fill table body..
+      if( c < Runden['Schuss']-1) {
+        // save shot result
+        r.push(Runden['Ring']);
+        x.push(Runden['xPos']);
+        y.push(Runden['yPos']);
+        c++;
+      } else {
+        r.push(Runden['Ring']);
+        x.push(Runden['xPos']);
+        y.push(Runden['yPos']);
+        tbody.insertRow(i);
+        // insert round data
+        let j=1;
+        tbody.rows[i].insertCell(0).innerText = i+1;
+	let rdate = Runden[passeCols[1]].replaceAll("-","_");
+        tbody.rows[i].insertCell(1).innerHTML = '<button onmouseover="showTrefferBild(['+x+'],['+y+'],['+r+'],['+i+'],['+rdate+'])">'+Runden[passeCols[j]]+'</button>';
+        for( let k=2; k<(passeCols.length-Runden['Schuss']); k++){
+          tbody.rows[i].insertCell(k).innerText = Runden[passeCols[k]];
+        }
+        // add shot results for this passe
+        let l=(passeCols.length-Runden['Schuss']);
+        for( let sc=0; sc < r.length; sc++) {
+            tbody.rows[i].insertCell(l++).innerHTML = '<img src="./img/' + r[sc] + '-50.png" alt="R:'+r[sc]+'">';
+        }
+        i++;
+        r=[];
+        x=[];
+        y=[];
+        c=0;
+      }
+    }
+    if( rid > 0){
+      passeInfoMid.appendChild(passeTable);
+    }
+  };
+  window.showPasseInfoMid = showPasseInfoMid;
 
   function showPasseInfoRight (rid, rnum) {
     passeInfoRight.innerHTML = '';
